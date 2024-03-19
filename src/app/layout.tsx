d6d4abf941fd9fbbe18b47157406e857eea3ea69
Angelo Guerra <jose_maria_guerra@dlsu.edu.ph>
2024-03-19 22:34:45 +0800

    Fixed modularization on homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,4 @@
-import HomeNavbar from "@/components/HomeNavbar";
-import Banner from "@/components/Banner";
 import "@/styles/globals.css";
-import { Inter } from "next/font/google";
-import { cn } from "@/lib/utils";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Toolbox",
@@ -12,26 +6,18 @@ export const metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode;
 }) {
   return (
-      <html
-          lang="en"
-          className={cn(
-              "bg-white-smoke text-black-pearl antialiased light",
-              inter.className
-          )}
-      >
-          <body className="min-h-screen pt-12 bg-white-smoke text-black-pearl antialiased">
-              <Banner/>
-              <HomeNavbar />
+      <html lang="en">
+      <body className="min-h-screen bg-white-smoke text-black-pearl antialiased">
 
-            <div className="container max-w-7xl mx-auto h-full pt-12">
-                {children}
-            </div>
-          </body>
+      <div className="container max-w-7xl h-full pt-12">
+        {children}
+      </div>
+      </body>
       </html>
   );
 }
